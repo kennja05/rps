57e7061e96bf4575fcd12d39f5cd37f82afaf81b
Jacob Kenny <jacobkenny05@gmail.com>
2022-11-15 09:24:43 -0500

    update game loop

--- rps.py
+++ rps.py
@@ -1,23 +1,30 @@
 from random import randint
 
 plays = ["rock", "paper", "scissors"]
-
 computer = plays[randint(0,2)]
 player = False
-wonMatchup = {
+wonMatch = {
     "rock": "scissors",
     "paper": "rock",
     "scissors": "paper"
 }
+
 while not player:
     player = input("Rock, Paper, or Scissors?\n").lower()
+    #check for valid entry
+    while player not in plays:
+        player = False
+        print("Invalid entry. Try again")
+        player = input("Rock, Paper, or Scissors?\n").lower()
+    #check for tie and use wonMatch dict if winner
     if player == computer:
         print("Tie!")
-    elif wonMatchup[player] == computer:
-        print("You Win") 
-    elif wonMatchup[computer] == player:
-        print("You lose")
+    elif wonMatch[player] == computer:
+        print("You Win!") 
     else:
-        print("Invalid entry")
-player = False
-computer = plays[randint(0,2)]
+        print("You lose!")
+    #option to loop through and play again
+    playAgain = input("Play again? y/n: ")
+    if playAgain == 'y':
+        player = False
+        computer = plays[randint(0,2)]
